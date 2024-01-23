64822ce189124fbc8a281b3758f14efb44ab72e0
Benjamin <benjamin.sokolov19@gmail.com>
2024-01-23 13:23:14 +0300

    исправил утечку памяти

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,30 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
 
 const placesList = document.querySelector(".places__list");
 
-function createCards() {
-  let cardList = [];
-  initialCards.forEach((element) => {
-    const cardTemplate = document.querySelector("#card-template").content;
-    const cardElement = cardTemplate
-      .querySelector(".places__item")
-      .cloneNode(true);
+function createCard(element) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
-    const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
 
-    const cardImage = cardElement.querySelector(".card__image");
-    cardImage.src = element.link;
-    cardImage.alt = element.name;
+  cardTitle.textContent = element.name;
 
-    const cardTitle = cardElement.querySelector(".card__title");
-    cardTitle.textContent = element.name;
-
-    deleteButton.addEventListener("click", deleteCard(deleteButton));
-    cardList.push(cardElement);
-  });
-  return cardList;
-}
-
-function createCardElements(deleteCard) {
-  const cards = createCards(deleteCard);
-  return cards;
+  deleteButton.addEventListener("click", () => deleteCard(deleteButton));
+  return cardElement;
 }
 
 function deleteCard(deleteButton) {
-  deleteButton.addEventListener("click", () => {
-    const listItem = deleteButton.closest(".card");
-    listItem.remove();
-  });
+  const listItem = deleteButton.closest('.card');
+  listItem.remove();
 }
 
 function postCards() {
-  const cardList = createCardElements(deleteCard);
-  cardList.forEach((element) => {
-    placesList.append(element);
+  initialCards.forEach((element) => {
+    placesList.append(createCard(element));
   });
 }
 
