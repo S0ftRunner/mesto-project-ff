3097575b5890bad6a6db1f54134b5192f1b2ebd7
S0ftRunner <benjamin.sokolov19@gmail.com>
2024-02-26 18:49:27 +0300

    добавил открытие и закрытие карточек

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import "./pages/index.css";
 import initialCards from "./cards";
 const placesList = document.querySelector(".places__list");
 
+const profileAddButton = document.querySelector(".profile__add-button");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popupTypeEdit = document.querySelector(".popup_type_edit");
+
+const closePopupsButtons = document.querySelectorAll(".popup__close");
+
+profileAddButton.addEventListener("click", () => openAddCardPopup());
+profileEditButton.addEventListener("click", () => openEditProfilePopup());
+
+closePopupsButtons.forEach((closeButton) => {
+  closeButton.addEventListener("click", () => closePopup(closeButton));
+});
+
 function createCard(element) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.cloneNode(true);
@@ -30,3 +45,16 @@ function postCards() {
 }
 
 postCards();
+
+function openAddCardPopup() {
+  popupNewCard.classList.add("popup_is-opened");
+}
+
+function openEditProfilePopup() {
+  popupTypeEdit.classList.add("popup_is-opened");
+}
+
+function closePopup(closeButton) {
+  const popup = closeButton.closest(".popup");
+  popup.classList.remove("popup_is-opened");
+}
